e758bc9924aa83d3bf70005e5e765d6e5f913fce
Dylanoco <dylanoco.official@gmail.com>
2023-08-13 12:08:08 +0100

    Blank Slate with Imports

--- main.py
+++ main.py
@@ -2,9 +2,3 @@ from task import Task
 from checklist import Checklist
 import tkinter as tk
 from tkinter import *
-
-root = Tk()
-root.title('To Do List ZYN')
-root.geometry("400x400")
-my_cl = Checklist(root)
-root.mainloop()
