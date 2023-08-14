5a56bda0d7a9f85f0c2bcd6959a366fd1497c2de
Dylanoco <dylanoco.official@gmail.com>
2023-08-14 13:27:27 +0100

    Deleted some unnecessary files

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from task import Task
-from checklist import Checklist
-import tkinter as tk
-from tkinter import *
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, template_folder="templates")
+
+todos = [{"task_name": "Sample", "done": False}]
+
+@app.route("/")
+def index():
+    return render_template("index.html", todos=todos)
+if __name__ == '__main__':
+    app.run(debug=True)
