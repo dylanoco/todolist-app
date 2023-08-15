4ad23aec9c0737db89f05c8358cbd85d6cb612d5
Dylanoco <dylanoco.official@gmail.com>
2023-08-15 09:37:00 +0100

    Added the 'Complete Task' Action.

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ todos = [{"task_name": "Sample", "done": False}]
 @app.route("/")
 def index():
     return render_template("index.html", todos=todos)
+
+@app.route("/update/<name>", methods=['GET'])
+def update_array(name):
+    for todo in todos:
+        if todo['task_name'] == name:
+            todo["done"] = True
+            break  # Assuming each task name is unique
+
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True)
