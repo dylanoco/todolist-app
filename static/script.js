aefc07b55cff69e6715a3ddd750e1108e1cc1113
Dylanoco <dylanoco.official@gmail.com>
2023-09-01 14:38:28 +0100

    Fixed Alignment on Top of Todolist. Added 'Add Task' Button.

--- static/script.js
+++ static/script.js
@@ -5,3 +5,15 @@ for (i = 0; i<accordion.length;i++){
         this.classList.toggle('active')
     })
 }
+
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup-bg").style.display = "flex";
+})
+
+document.querySelector(".popup-bg").addEventListener("click", function (e) {
+    // Check if the clicked element is the popup content or a child of it
+    if (!e.target.closest(".popup-content")) {
+        document.querySelector(".popup-bg").style.display = "none";
+    }
+})
